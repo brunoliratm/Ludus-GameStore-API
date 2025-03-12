12a253079a1f210d072fa44c72ff8ac4faae2a16
brunoliratm <bmagnoserver@gmail.com>
2025-03-11 23:11:49 -0300

    Add UserRepository and UserService for user management functionality

--- src/main/java/com/ludus/repository/UserRepository.java
+++ src/main/java/com/ludus/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.ludus.repository;
 
-import com.ludus.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.ludus.models.UserModel;
 
 @Repository
-public interface userRepository extends JpaRepository<User, String> {
-    User findByEmail(String email);
+public interface UserRepository extends JpaRepository<UserModel, String> {
+    UserModel findByEmail(String email);
 }
