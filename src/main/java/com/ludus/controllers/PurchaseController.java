b8b758936c1c514bd64a2b7fd2ef480cc5de4965
BrunoMagno <bmagnoserver@gmail.com>
2025-04-02 08:42:24 -0300

    Implement PurchaseService and PurchaseController with CRUD operations; enhance PurchaseDTO with validation; update PurchaseModel to include price and user association.

--- src/main/java/com/ludus/controllers/PurchaseController.java
+++ src/main/java/com/ludus/controllers/PurchaseController.java
@@ -1,6 +1,7 @@
 package com.ludus.controllers;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.ludus.dto.PurchaseDTO;
+import com.ludus.services.PurchaseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
@@ -16,29 +18,34 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RequestMapping("/api/purchases")
 public class PurchaseController {
 
+  @Autowired
+  private PurchaseService purchaseService;
+
   @Operation(summary = "Get All Purchases")
   @GetMapping()
   public ResponseEntity<List<PurchaseDTO>> getPurchases() {
-    return new ResponseEntity<>(HttpStatus.OK);
+    List<PurchaseDTO> purchases = purchaseService.getAllPurchases();
+    return new ResponseEntity<>(purchases, HttpStatus.OK);
   }
 
   @Operation(summary = "Get a Purchase by ID")
   @GetMapping("/{id}")
   public ResponseEntity<PurchaseDTO> getPurchase(@PathVariable Long id) {
-    return new ResponseEntity<>(HttpStatus.OK);
+    PurchaseDTO purchase = purchaseService.getPurchase(id);
+    return new ResponseEntity<>(purchase, HttpStatus.OK);
   }
 
   @Operation(summary = "Create a New Purchase")
   @PostMapping()
   public ResponseEntity<Void> createPurchase(@RequestBody PurchaseDTO purchaseDTO) {
+    purchaseService.createPurchase(purchaseDTO);
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
   @Operation(summary = "Get a Purchase by user")
   @GetMapping("/user/{id}")
   public ResponseEntity<List<PurchaseDTO>> getPurchasesByUser(@PathVariable Long id) {
-    return new ResponseEntity<>(HttpStatus.OK);
+    List<PurchaseDTO> purchases = purchaseService.getPurchasesByUser(id);
+    return new ResponseEntity<>(purchases, HttpStatus.OK);
   }
-
-
 }
