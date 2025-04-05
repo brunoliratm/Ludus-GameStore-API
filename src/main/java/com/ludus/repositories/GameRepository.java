7102ba277ce365477535f292156d8448132cbd5b
BrunoMagno <bmagnoserver@gmail.com>
2025-04-05 12:00:35 -0300

    Refactor repositories to use enum types for genre and payment method in method signatures

--- src/main/java/com/ludus/repositories/GameRepository.java
+++ src/main/java/com/ludus/repositories/GameRepository.java
@@ -3,13 +3,14 @@ package com.ludus.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.ludus.enums.GameGenre;
 import com.ludus.models.GameModel;
 
 public interface GameRepository extends JpaRepository<GameModel, Long> {
 
-    Page<GameModel> findByGenreAndName(String genre, String name, Pageable pageable);
+    Page<GameModel> findByGenreAndName(GameGenre genre, String name, Pageable pageable);
 
-    Page<GameModel> findByGenre(String genre, Pageable pageable);
+    Page<GameModel> findByGenre(GameGenre genre, Pageable pageable);
 
     Page<GameModel> findByName(String name, Pageable pageable);
 
